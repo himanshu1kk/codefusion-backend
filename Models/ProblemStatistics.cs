6a44d9aee7b174ecfdc1fa50426d81956f9158da
himanshu1kk <kandpalhimanshu84@gmail.com>
2025-12-25 11:35:01 +0530

    added search ques

---
+++ Models/ProblemStatistics.cs
@@ -0,0 +1,9 @@
+namespace CFFFusions.Models
+{
+    public class ProblemStatistics
+    {
+        public int? ContestId { get; set; }
+        public string Index { get; set; }
+        public int SolvedCount { get; set; }
+    }
+}
---
+++ Models/ProblemsetResponse.cs
@@ -0,0 +1,8 @@
+namespace CFFFusions.Models
+{
+    public class ProblemsetResponse
+    {
+        public List<Problem> Problems { get; set; }
+        public List<ProblemStatistics> ProblemStatistics { get; set; }
+    }
+}
